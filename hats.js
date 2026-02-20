b534360f1ce76a923f7cb0259d5808e16b7bfa1c
Rylie Carroll <80358171+ryliecarroll@users.noreply.github.com>
2026-02-20 14:23:19 -0500

    Add files via upload

---
+++ hats.js
@@ -0,0 +1,74 @@
+const hatsData = [
+    {
+        "id": 0,
+        "name": "Roof Jesus",
+        "price": "$70.00",
+        "type": "3d",
+        "src": "https://modelviewer.dev/shared-assets/models/Astronaut.glb",
+        "description": "100% Cotton"
+    },
+    {
+        "id": 1,
+        "name": "zishou",
+        "price": "$60.00",
+        "type": "image",
+        "src": "https://placehold.co/200x200/222/FFF?text=zishou",
+        "description": "100% Polyester"
+    },
+        {
+        "id": 2,
+        "name": "zishou2",
+        "price": "$60.00",
+        "type": "image",
+        "src": "https://placehold.co/200x200/222/FFF?text=zishou",
+        "description": "100% Polyester"
+    },
+        {
+        "id": 3,
+        "name": "zishou3",
+        "price": "$60.00",
+        "type": "image",
+        "src": "https://placehold.co/200x200/222/FFF?text=zishou",
+        "description": "100% Polyester"
+    },
+            {
+        "id": 4,
+        "name": "zishou4",
+        "price": "$60.00",
+        "type": "image",
+        "src": "https://placehold.co/200x200/222/FFF?text=zishou",
+        "description": "100% Polyester"
+    },
+                {
+        "id": 5,
+        "name": "zishou5",
+        "price": "$60.00",
+        "type": "image",
+        "src": "https://placehold.co/200x200/222/FFF?text=zishou",
+        "description": "100% Polyester"
+    },
+                    {
+        "id": 6,
+        "name": "zishou6",
+        "price": "$60.00",
+        "type": "image",
+        "src": "https://placehold.co/200x200/222/FFF?text=zishou",
+        "description": "100% Polyester"
+    },
+                        {
+        "id": 7,
+        "name": "zishou7",
+        "price": "$60.00",
+        "type": "image",
+        "src": "https://placehold.co/200x200/222/FFF?text=zishou",
+        "description": "100% Polyester"
+    },
+                            {
+        "id": 8,
+        "name": "zishou8",
+        "price": "$60.00",
+        "type": "image",
+        "src": "https://placehold.co/200x200/222/FFF?text=zishou",
+        "description": "100% Polyester"
+    },
+];
